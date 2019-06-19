6621868339d00d3578c8ed9850608585bb85f932
Eduardo Navas <enavas@shtvsolution.com>
2019-06-19 20:32:51 +0200

    The node module localtunnel was added to the project and minor modifications were made to the code so that the server can be remotely accessed through the url "https://droidmare-api.localtunnel.me".

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //Strict mode ensures that sintax errors won't be allowed:
 'use strict'
 
+//The node localtunnel module is imported in order to assign a public subdomain to localhost:3000:
+const localtunnel = require('localtunnel');
+
 //The node mongoose module is imported so mongoDB can be used:
 const mongoose = require('mongoose');
 
@@ -12,14 +15,22 @@ const databasePort = 27017;
 const serverPort = 3000;
 const url = 'mongodb://localhost:' + databasePort + '/usersDB';
 
-//Now the database connection is established, starting also the server if the mongoDB was succcesfully created:
-mongoose.connect(url, { useNewUrlParser: true })
-  .then(() => {
-    console.log('Database initialized!');
-    app.listen(serverPort, () => {
-        console.log('Server listening on port ' + serverPort);
+//Before the server is started, the public subdomain 'droidmare-api' is asigned to localhost:
+localtunnel(serverPort, {subdomain: 'droidmare-api'}, (err, tunnel) => {
+  //The server will be started only if the tunnel is successfully created:
+  if (err) console.log('Could not create tunnel!');
+  else {
+    console.log('Tunnel successfully created. Public domain: ' + tunnel.url);
+    //Now the database connection is established, starting also the server if the mongoDB was succcesfully created:
+    mongoose.connect(url, { useNewUrlParser: true })
+    .then(() => {
+      console.log('Database initialized!');
+      app.listen(serverPort, () => {
+          console.log('Server listening on port ' + serverPort);
+      });
+    })
+    .catch(err =>  {
+      console.log(err);
     });
-  })
-  .catch(err =>  {
-    console.log(err);
-  });
+  }
+});
